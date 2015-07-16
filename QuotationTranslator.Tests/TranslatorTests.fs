ea69be3a55ee1e2125d7bfe5cc1aa2d6a81856f9
Daniil Smirnov <danslapman@gmail.com>
2015-07-16 13:02:28 +0300

    Init

---
+++ QuotationTranslator.Tests/TranslatorTests.fs
@@ -0,0 +1,12 @@
+﻿module TranslatorTests
+
+open Translator
+open Xunit
+
+type TestEntity(id) =
+    member val Id = id with get, set
+
+[<Fact>]
+let SimpleGetterTest() =
+    let expr = getProperty <@ fun (x: TestEntity) -> x.Id @>
+    ()
---
+++ QuotationTranslator/Translator.fs
@@ -0,0 +1,22 @@
+﻿module Translator
+
+open Microsoft.FSharp.Quotations
+open System.Linq.Expressions
+open System
+open Microsoft.FSharp.Quotations.Patterns
+
+/// <summary>
+/// Converts quotation with lambda property getter into
+/// MemberAccessExpression wrapped into LambdaExpression
+/// </summary>
+/// <param name="expr"></param>
+let getProperty (expr: Expr<'a -> 'b>): Expression<Func<'a, 'b>> =
+    match expr with
+    | Lambda(_, body) ->
+        match body with
+        | PropertyGet(_, pinfo, _) ->
+            let arg = Expression.Parameter(typeof<'a>)
+            let getter = Expression.Property(arg, pinfo) :> Expression
+            Expression.Lambda<Func<'a, 'b>>(getter, arg)
+        | _ -> failwith "getProperty translator accepts only quotations with property getters"
+    | _ -> failwith "Quotation does not contain lambda function"
